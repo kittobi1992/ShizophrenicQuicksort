995a1742bf3b6e6de3449a779a12c52fc514c82b
kittobi92 <Tobias.Heuer@gmx.net>
2016-01-09 11:06:24 +0100

    Test input

---
+++ input/gen.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  
+  int N = 1000; int MAX_NUM = 500;
+  for(int i = 0; i < N; i++) {
+    cout << rand() % MAX_NUM << (i == N ? "" : " ");
+  }
+  cout << endl;
+  
+  return 0;
+}
