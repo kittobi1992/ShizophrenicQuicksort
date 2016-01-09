fef6068c5e19ea7160321820118df33d7a7e218c
kittobi92 <Tobias.Heuer@gmx.net>
2016-01-09 13:31:05 +0100

    read data and scatter it to all processes (test)

--- input/gen.cpp
+++ input/gen.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
   
   int N = 1000; int MAX_NUM = 500;
+  cout << N << endl;
   for(int i = 0; i < N; i++) {
     cout << rand() % MAX_NUM << (i == N ? "" : " ");
   }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include <mpi.h>
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main(int argc, char** argv) {
     // Initialize the MPI environment
@@ -12,6 +14,21 @@ int main(int argc, char** argv) {
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    
+    int N, *data, *pdata; 
+    if(world_rank == 0) {
+      cin >> N;
+      cout << N << endl;
+      data = (int *) malloc(N*sizeof(int));
+      for(int i = 0; i < N; i++) {
+	cin >> data[i];
+      }
+    }
+    
+    MPI_Bcast(&N,1,MPI_INT,0,MPI_COMM_WORLD);
+    int split_size = ceil(static_cast<double>(N)/world_size);
+    pdata = (int *) malloc(split_size*sizeof(int));
+    MPI_Scatter(data,split_size,MPI_INT,pdata,split_size,MPI_INT,0,MPI_COMM_WORLD);
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -19,9 +36,11 @@ int main(int argc, char** argv) {
     MPI_Get_processor_name(processor_name, &name_len);
 
     // Print off a hello world message
-    printf("Hello world from processor %s, rank %d"
-           " out of %d processors\n",
-           processor_name, world_rank, world_size);
+    cout << "Elements of processor " << world_rank << ": " << endl;
+    for(int i = 0; i < split_size; i++) {
+      cout << pdata[i] << " ";
+    }
+    cout << endl;
 
     // Finalize the MPI environment.
     MPI_Finalize();
